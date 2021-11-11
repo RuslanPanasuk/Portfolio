c7963331a2b3fa1f6602e118b10b2de160cc44be
RuslanPanasuk <panasuk.eetp@gmail.com>
2021-11-11 23:03:38 +0200

    feat: add portfolio section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const close = document.querySelector('.menu__close');
+const levels = document.querySelectorAll('.skils__indicator-level');
+const levelBars = document.querySelectorAll('.skils__indicator-bar-level');
 
 hamburger.addEventListener('click', () => {
   menu.classList.add('active');
@@ -10,3 +12,6 @@ close.addEventListener('click', () => {
   menu.classList.remove('active');
 });
 
+levels.forEach((item, i) => {
+  levelBars[i].style.width = item.innerHTML;
+})
